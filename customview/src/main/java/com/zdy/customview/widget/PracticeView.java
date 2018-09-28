325de2ec5df0fd3c2f720cd43b72cbae41e96234
DESKTOP-JBJ463C\shiming <364112371@qq.com>
2018-09-28 18:38:42 +0800

    绘制文字联系

--- customview/src/main/java/com/zdy/customview/widget/PracticeView.java
+++ customview/src/main/java/com/zdy/customview/widget/PracticeView.java
@@ -6,12 +6,17 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
 /**
  * 练习View
- * */
+ */
 public class PracticeView extends View {
-    Paint mPaint;
+    Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint paint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public PracticeView(Context context) {
         this(context, null);
@@ -27,17 +32,58 @@ public class PracticeView extends View {
     }
 
     private void initPaint() {
-        mPaint = new Paint();
-        mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setAntiAlias(true);
-        mPaint.setColor(Color.BLACK);
+        paint1.setTextSize(60);
+        paint2.setTextSize(120);
+        paint3.setTextSize(60);
+        paint2.setColor(Color.parseColor("#E91E63"));
+        //测量文字宽度必须要在设置文字大小或其他改变大小的API之后
+        measuredText1 = paint1.measureText(text1);
+        measuredText2 = paint2.measureText(text2);
+        measuredText3 = paint3.measureText(text3);
+        Log.e("measuredText1", measuredText1 + "");
+        Log.e("measuredText2", measuredText2 + "");
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 //        drawCircle(canvas);
-        drawDrawArc(canvas);
+//        drawDrawArc(canvas);
+        drawText(canvas);
+    }
+
+    String text1 = "三个月内你胖了";
+    String text2 = "4.5";
+    String text3 = "公斤";
+    float measuredText1;
+    float measuredText2;
+    float measuredText3;
+//    private Rect contentBounds = new Rect();
+//    private Rect numBounds = new Rect();
+
+    private void drawText(Canvas canvas) {
+//        float contentWidth = mPaint.measureText(content);
+//        float numWidth = mPaint.measureText(num);
+//        Log.e("contentWidth", contentWidth + "");
+//        Log.e("numWidth", numWidth + "");
+//        mPaint.setColor(Color.BLACK);
+//        mPaint.setTextSize(50);
+//        mPaint.getTextBounds(content, 0, content.length(), contentBounds);
+//        canvas.drawText(content, 50, 200, mPaint);
+//        Log.e("TextBounds", contentBounds.width() + "");
+
+//        mPaint.setColor(Color.RED);
+//        mPaint.setTextSize(90);
+//        mPaint.getTextBounds(num, 0, num.length(), numBounds);
+//        canvas.drawText(num, 50 + contentBounds.width(), 200, mPaint);
+
+//        mPaint.setColor(Color.BLACK);
+//        mPaint.setTextSize(50);
+//        canvas.drawText("公斤", 50 + contentBounds.width() + numBounds.width(), 200, mPaint);
+
+        canvas.drawText(text1, 50, 200, paint1);
+        canvas.drawText(text2, 50 + measuredText1 , 200, paint2);
+        canvas.drawText(text3, 50 + measuredText1  + measuredText2 , 200, paint3);
     }
 
     private void drawDrawArc(Canvas canvas) {
