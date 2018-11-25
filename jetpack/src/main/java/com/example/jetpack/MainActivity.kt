adaa18d1f34fb9d2f225ea8ef90bdd72c9473057
lixiangguo <364112371@qq.com>
2018-11-25 23:00:38 +0800

    加入新模块

---
+++ jetpack/src/main/java/com/example/jetpack/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.jetpack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *   created by co753 on 2018/11/25
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
