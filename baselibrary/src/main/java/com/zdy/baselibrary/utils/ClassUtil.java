ea68448af9e5d5c0884f9323de23f3306b9243a0
理想国 <364112371@qq.com>
2019-06-06 17:17:30 +0800

    完善反射API

--- baselibrary/src/main/java/com/zdy/baselibrary/utils/ClassUtil.java
+++ baselibrary/src/main/java/com/zdy/baselibrary/utils/ClassUtil.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 /**
  * 反射相关的API
- * */
+ */
 public class ClassUtil {
     public static void getClassInfo(Object obj) {
         Class<?> c = obj.getClass();
@@ -52,6 +52,183 @@ public class ClassUtil {
             System.out.println("list1:" + list1.toString());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        Class<? extends Test> testClass = test.getClass();
+        //返回类的所有构造方法
+//        Constructor<?>[] constructors = testClass.getDeclaredConstructors();
+        //返回类中所有 public 类型的构造方法
+//        Constructor<?>[] constructors = testClass.getConstructors();
+//        for (int i = 0; i < constructors.length; i++) {
+//            System.out.print(Modifier.toString(constructors[i].getModifiers()) + "参数：");
+//            Class<?>[] parameterTypes = constructors[i].getParameterTypes();
+//            for (int j = 0; j < parameterTypes.length; j++) {
+//                System.out.print(parameterTypes[j].getName() + " ");
+//            }
+//            System.out.println("");
+//        }
+        /**
+         * 执行结果：
+         * public参数：
+         * private参数：java.lang.String
+         * public参数：int java.lang.String
+         * public参数：int
+         * */
+
+        /**
+         * 获取特定参数类型的构造方法,包括私有方法
+         * */
+//        try {
+//            //获取特定参数类型的构造方法,包括私有方法,获取无参构造方法直接不传参数,获取对应参数的构造方法就传入对应参数
+//            Constructor<? extends Test> constructors = testClass.getDeclaredConstructor();
+//            System.out.print(Modifier.toString(constructors.getModifiers()));
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        }
+        /**
+         * public
+         * */
+//        try {
+//            Constructor<? extends Test> constructors = testClass.getDeclaredConstructor(int.class, String.class);
+//            System.out.print(Modifier.toString(constructors.getModifiers()));
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        }
+        /**
+         * public
+         * */
+//        try {
+//            Constructor<? extends Test> constructors = testClass.getDeclaredConstructor(String.class);
+//            System.out.print(Modifier.toString(constructors.getModifiers()) + " 参数：");
+//            Class<?>[] parameterTypes = constructors.getParameterTypes();
+//            for (int i = 0; i < parameterTypes.length; i++) {
+//                System.out.print(parameterTypes[i].getName() + " ");
+//            }
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        }
+        /**
+         * private 参数：java.lang.String
+         * */
+
+        /**
+         * 反射调用构造方法
+         * */
+//        try {
+            //根据参数来决定调用哪个构造方法
+//            Constructor<? extends Test> constructor = testClass.getDeclaredConstructor(int.class, String.class);
+//            constructor.newInstance(24,"啦啦啦");
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
+
+        /**
+         * 反射执行私有构造方法
+         * */
+//        try {
+//            //反射执行私有构造方法
+//            Constructor<? extends Test> constructor = testClass.getDeclaredConstructor(String.class);
+//            constructor.setAccessible(true);//不加这行反射私有构造函数会报 IllegalAccessException 错
+//            constructor.newInstance("啦啦啦");
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
+
+        /**
+         * 调用类的私有方法
+         * */
+//        try {
+//            Method method = testClass.getDeclaredMethod("welcome", String.class);
+//            //调用无参构造函数,创建一个实例
+////            Test instance = testClass.newInstance();
+//            method.setAccessible(true);
+//            //执行方法，第一个参数是传入方法所在类的实例，第二个参数是传入方法的参数
+//            method.invoke(test,"反射调用私有方法");
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
+
+//        获取类的私有字段并修改值
+        try {
+            Field field = testClass.getDeclaredField("name");
+            field.setAccessible(true);
+            field.set(test,"啦啦啦");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println(test.getName());
+    }
+
+    public static class Test {
+        private int age;
+        private String name;
+        private int testint;
+
+        public Test(int age) {
+            this.age = age;
+        }
+
+        public Test(int age, String name) {
+            this.age = age;
+            this.name = name;
+            System.out.println("我是公开构造函数，hello" + name + " i am" + age);
+        }
+
+        private Test(String name) {
+            this.name = name;
+            System.out.println("我是私有构造函数，My Name is" + name);
+        }
+
+        public Test() {
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getTestint() {
+            return testint;
+        }
+
+        public void setTestint(int testint) {
+            this.testint = testint;
+        }
+
+        private void welcome(String tips){
+            System.out.println(tips);
         }
     }
 }
